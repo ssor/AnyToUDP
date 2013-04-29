7e638cef30413c780d731a766c999881edc721a6
ssor <ssor@qq.com>
2013-04-29 13:10:33 +0800

    因为使用，更了一下，不涉及类的修改

--- AnyToUDP/Form1.cs
+++ AnyToUDP/Form1.cs
@@ -18,7 +18,7 @@ namespace AnyToUDP
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Protocol2UDP p2u = Protocol2UDP.getProtocol2UDP(new SerialPort2UDP("com10", 9600), "192.168.1.104", 4001);
+            Protocol2UDP p2u = Protocol2UDP.getProtocol2UDP(new SerialPort2UDP("com4", 19200), "192.168.1.100", 5000);
             if (p2u != null)
             {
                 p2u.startRun();
